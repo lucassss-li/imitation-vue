5b53b320b87aff587afa86ae70f1f48afd68b306
lucassss-li <lucas8qwer@gmail.com>
2022-04-11 20:48:04 +0800

    [improve]effect/base

--- improve/reactivity/baseHandlers.ts
+++ improve/reactivity/baseHandlers.ts
@@ -1,3 +1,4 @@
+import { track, trigger } from './effect'
 import { ReactiveFlags, reactiveMap, reactive } from './reactive'
 
 export const mutableHandlers: ProxyHandler<object> = {
@@ -10,9 +11,12 @@ export const mutableHandlers: ProxyHandler<object> = {
         ) {
             return target
         }
+        track(target, key)
         return reactive(Reflect.get(target, key, receiver))
     },
     set(target, key, value, receiver) {
-        return Reflect.set(target, key, value, receiver)
+        const res = Reflect.set(target, key, value, receiver)
+        trigger(target, key)
+        return res
     },
 }
