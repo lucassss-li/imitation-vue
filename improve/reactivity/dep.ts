0a34624e0e08a077637cf6e6d2cbf5452465a0cc
lucassss-li <lucas8qwer@gmail.com>
2022-04-19 15:36:51 +0800

    reset improve

--- improve/reactivity/dep.ts
+++
@@ -1,8 +0,0 @@
-import { ReactiveEffect } from './effect'
-
-export type Dep = Set<ReactiveEffect>
-
-export const createDep = (effects?: ReactiveEffect[]): Dep => {
-    const dep = new Set<ReactiveEffect>(effects) as Dep
-    return dep
-}
