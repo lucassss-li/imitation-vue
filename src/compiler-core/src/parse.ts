3a3295e7345fe38296994cb297259ab1f1c82453
lucassss-li <lucas8qwer@gmail.com>
2022-05-30 21:28:40 +0800

    [compiler-core][parseElement]

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from './ast'
 
+const enum TagType {
+    START,
+    END,
+}
+
 export function baseParse(content: string) {
     const context = createParserContext(content)
     return createRoot(parseChildren(context))
@@ -7,11 +12,33 @@ export function baseParse(content: string) {
 
 function parseChildren(context) {
     const nodes: any[] = []
+    let node
     if (context.source.startsWith('{{')) {
-        const node = parseInterpolation(context)
-        nodes.push(node)
+        node = parseInterpolation(context)
+    } else if (context.source[0] === '<') {
+        if (/[a-z]/i.test(context.source[1])) {
+            node = parseElement(context)
+        }
     }
+    nodes.push(node)
     return nodes
+}
+
+function parseElement(context) {
+    const element = parseTag(context, TagType.START)
+    parseTag(context, TagType.END)
+    return element
+}
+
+function parseTag(context, type: TagType) {
+    const match: any = /^<\/?([a-z]+?)>/i.exec(context.source)
+    const tag = match[1]
+    advanceBy(context, tag.length + 2)
+    if (type === TagType.END) return
+    return {
+        type: NodeTypes.ELEMENT,
+        tag,
+    }
 }
 
 function parseInterpolation(context) {
