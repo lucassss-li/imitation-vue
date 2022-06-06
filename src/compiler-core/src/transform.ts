5adba0b662d6ed6439d676f2c11b842dd3a2b1f4
lucassss-li <lucas8qwer@gmail.com>
2022-06-06 20:28:53 +0800

    [compiler-core][transform]

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,24 @@
+export function transform(root, options) {
+    const context = createTransformContext(root, options)
+    traverseNode(root, context)
+}
+
+function createTransformContext(root, options) {
+    const context = {
+        root,
+        nodeTransforms: options.nodeTransforms || [],
+    }
+    return context
+}
+
+function traverseNode(node, context) {
+    const nodeTransforms = context.nodeTransforms
+    for (let i = 0; i < nodeTransforms.length; i++) {
+        nodeTransforms[i](node)
+    }
+    if (node.children) {
+        for (let i = 0; i < node.children.length; i++) {
+            traverseNode(node.children[i], context)
+        }
+    }
+}
